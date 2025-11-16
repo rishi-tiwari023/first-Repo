b824dfe9da5dfc1f013ff2da3187e58cbb177ab8
Rishi Tiwari <umesh896073@gmail.com>
2025-11-16 22:14:04 +0530

    Add Product component having title, description and Price

--- reactLearning/frontend-basic/src/Product.jsx
+++ reactLearning/frontend-basic/src/Product.jsx
@@ -1,8 +1,15 @@
 import "./Product.css"
-function Product({title, price}) {
+import Price from "./Price.jsx";
+function Product({title, idx}) {
+    let description=[["8,000 DPI", "5 Programmable Buttons"],
+                ["intutive surface","designed for iPad Pro"],
+                ["designed for iPad Pro","intutive surface"],
+                ["wireless","optical orientation"]]
     return <div className="Product">
-    <h2>Product Title: {title}</h2>
-    <p>Price: &#8377;{price}</p>
+    <h3>{title}</h3>
+    <p>{description[idx][0]}</p>
+    <p>{description[idx][1]}</p>
+    <Price idx={idx} />
     </div>
 }
 
