391dce7f1afc08a955693120e40a38c4082faf7e
Rishi Tiwari <umesh896073@gmail.com>
2025-11-30 15:17:49 +0530

    Conditionals: 3. Using && to prevent extra node in DOM

--- reactLearning/react-props/src/Product.jsx
+++ reactLearning/react-props/src/Product.jsx
@@ -5,7 +5,8 @@ export default function Product({title,price})
         <div className="Product">
         <h4>{title}</h4>
         <p>Price : {price}</p>
-        <p> {price >5000 ? "Discount of 5% is Applied." : "NO Discount"}</p>
+    {price >5000 && <p> "Discount of 5% is Applied." </p>} 
+    {/* Conditional Rendering. This Doesn't render anything if the condition is false. No extra node in DOM. */}
         </div>
         )
 }
