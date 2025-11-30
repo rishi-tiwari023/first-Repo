c07a1f65ff2d58cd6795c2d4ca243dfdb7366b71
Rishi Tiwari <umesh896073@gmail.com>
2025-11-30 15:12:00 +0530

    Conditionals: 2. Directly using ternary operator

--- reactLearning/react-props/src/Product.jsx
+++ reactLearning/react-props/src/Product.jsx
@@ -1,24 +1,11 @@
 import "./Product.css";
 export default function Product({title,price}) 
 {
-    if(price>5000)
-    {
-        return (
+    return (
         <div className="Product">
         <h4>{title}</h4>
         <p>Price : {price}</p>
-        <p>Discount of 5% is Applied.</p>
+        <p> {price >5000 ? "Discount of 5% is Applied." : "NO Discount"}</p>
         </div>
         )
-    }
-    else
-    {
-        return (
-        <div className="Product">
-        <h4>{title}</h4>
-        <p>Price : {price}</p>
-        <p> No Discount is Applied.</p>
-        </div>
-        )
-    }
 }
